0e8e88821c996dde036fc6d876b3e4fdf963df0c
Jesse Willis <coloradopioneer@gmail.com>
2015-11-11 21:11:23 -0700

    added edit, update, show and index functions

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,7 +1,25 @@
 class GamesController < ApplicationController
 
+	def index
+		@games = Game.all
+	end
+
 	def new
 		@game = Game.new
+	end
+
+	def edit
+		@game = Game.find(params[:id])
+	end
+
+	def update
+		@game = Game.find(params[:id])
+		if @game.update(game_params)
+			flash[:notice] = "Game was successfully updated."
+			redirect_to game_path(@game)
+		else
+			render 'edit'
+		end
 	end
 
 	def create
