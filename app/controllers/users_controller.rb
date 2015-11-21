b880237df438f9fde261186e1ddcf336e24ecdbd
Jesse Willis <coloradopioneer@gmail.com>
2015-11-21 16:16:17 -0700

    auth methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+	before_action :set_user, only: [:edit, :update, :show]
 
 	def index
 		@users = User.paginate(page: params[:page], per_page: 2)
@@ -19,11 +20,10 @@ class UsersController < ApplicationController
 	end
 
 	def edit
-		@user = User.find(params[:id])
+
 	end
 
 	def update
-		@user = User.find(params[:id])
 		if @user.update(user_params)
 			flash[:success] = "Your account was updated"
 			redirect_to games_path
@@ -33,7 +33,6 @@ class UsersController < ApplicationController
 	end
 
 	def show
-		@user = User.find(params[:id])
 		@user_games = @user.games.paginate(page: params[:page], per_page: 5)
 	end
 
@@ -41,6 +40,10 @@ class UsersController < ApplicationController
 
 	def user_params
 		params.require(:user).permit(:username, :email, :password)
+	end
+
+	def set_user
+		@user = User.find(pramas[:id])
 	end
 
 end
